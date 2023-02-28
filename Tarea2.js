2c409f1c9a746fbeed693f0217b07d198d54d164
Alejandro Vergara Tamayo <73759053+Alejotamayo28@users.noreply.github.com>
2023-02-28 12:11:27 -0500

    Add files via upload

--- Tarea2.js
+++ Tarea2.js
@@ -1,26 +1,10 @@
-altura_centimetros = 172
-altura = 1.72
-peso = 54.3
-
-/*const hola = ( i) => {
-    console.log(i)
-    if (i < 2){
-        hola(2);
-    }
-    return 2;
-}
-
-console.log(hola(1));
-*/
-console.log(Math.round(altura))
-console.log(Math.floor(peso))
-
-let max_valor = Number.MAX_VALUE
-let aux = Number(1)
-let max_valor1 = Number.MAX_VALUE + aux
-
-if(max_valor === max_valor1){
-    console.log("Son numeros iguales")
-}else{
-    console.log("Son numeros diferentes")
+const num = 6;
+let x = 0,
+  y = 1,
+  z = 1;
+for (let i = 0; i <= num; i++) {
+  z = x + y; // 1 2 3 5
+  console.log(`${z} `); // 1 2 3 5  
+  x = y; // 1 1 2 3
+  y = z; // 1 2 3 5
 }
